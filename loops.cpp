9454fce229c771d5bdbe6cd0b304d0d04d3beeac
qiaosiyi <qsy2011815@sina.com>
2016-06-06 10:25:28 +0800

    Create loops.cpp

---
+++ loops.cpp
@@ -0,0 +1,35 @@
+#include <map>
+#include <set>
+#include <list>
+#include <cmath>
+#include <ctime>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <string>
+#include <bitset>
+#include <cstdio>
+#include <limits>
+#include <vector>
+#include <climits>
+#include <cstring>
+#include <cstdlib>
+#include <fstream>
+#include <numeric>
+#include <sstream>
+#include <iostream>
+#include <algorithm>
+#include <unordered_map>
+
+using namespace std;
+
+
+int main(){
+    int N;
+    cin >> N;
+    int i=1; 
+    for(i;i<11;i++){
+        cout<<N<<" x "<<i<<" = "<<N*i<<endl;
+    }
+    return 0;
+}
